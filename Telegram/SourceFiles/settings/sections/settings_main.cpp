bc9bde52cd7e692e1dd4e8ee2390cdd63a4341a9
Andrey <azimoff@rnbclub.ru>
2026-07-23 02:47:55 +0300

    fix: remove orphaned phoneNumberHidden subscription in settings cover

--- Telegram/SourceFiles/settings/sections/settings_main.cpp
+++ Telegram/SourceFiles/settings/sections/settings_main.cpp
@@ -292,11 +292,6 @@ void Cover::initViewers() {
 		updateIdText();
 	}, lifetime());
 
-	_user->session().settings().phoneNumberHiddenValue(
-	) | rpl::on_next([=] {
-		updatePhoneText();
-	}, lifetime());
-
 	Info::Profile::UsernameValue(
 		_user
 	) | rpl::on_next([=](const TextWithEntities &value) {
